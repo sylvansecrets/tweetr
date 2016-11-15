efbe6f50751cd9840a2bbb201b95643a7ee753b4
Richard Zhang <lapislacrima@gmail.com>
2016-11-15 18:05:10 +0000

    changed let to var for ES5 compatibility

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -16,7 +16,7 @@ function loadTweets(){
 }
 
 function renderTweets(tweets){
-  for (let user in tweets){
+  for (var user in tweets){
     tweetDOM = createTweetElement(tweets[user]);
     $('.tweet-contents').append(tweetDOM);
   }
@@ -24,15 +24,15 @@ function renderTweets(tweets){
 
 
 function createTweetElement(tweet){
-  let $tweet = $('<article>').addClass('tweet');
-  let image = `<img class='avatar' src=${escape(tweet['user']['avatars']['regular'])}>`;
-  let userName = `<h2>${escape(tweet['user']['name'])}</h2>`;
-  let userAvatar = `<h4>${escape(tweet['user']['handle'])}</h4>`
-  let head = `<header>${image+userName+userAvatar}</header>`
-  let tweetText = tweet['content']['text'];
-  let para = `<p class="tweet-text">${escape(tweetText)}</p>`
-  let timeStamp = printTime(tweet['created_at']);
-  let foot = `<footer><p class='tweet-time'>${escape(timeStamp)}</p></footer>`
+  var $tweet = $('<article>').addClass('tweet');
+  var image = `<img class='avatar' src=${escape(tweet['user']['avatars']['regular'])}>`;
+  var userName = `<h2>${escape(tweet['user']['name'])}</h2>`;
+  var userAvatar = `<h4>${escape(tweet['user']['handle'])}</h4>`
+  var head = `<header>${image+userName+userAvatar}</header>`
+  var tweetText = tweet['content']['text'];
+  var para = `<p class="tweet-text">${escape(tweetText)}</p>`
+  var timeStamp = printTime(tweet['created_at']);
+  var foot = `<footer><p class='tweet-time'>${escape(timeStamp)}</p></footer>`
   $($tweet).append(head);
   $($tweet).append(para);
   $($tweet).append(foot);
@@ -40,40 +40,40 @@ function createTweetElement(tweet){
 }
 
 function printTime(timeStamp){
-  let current = Date.now();
-  let diff = (timeStamp - current)/1000;
+  var current = Date.now();
+  var diff = (timeStamp - current)/1000;
   return roundTime(-1 * diff);
 }
 
 function roundTime(time){
-  let year = time/(365*24*60*60)
+  var year = time/(365*24*60*60)
   if(year > 1){
     return `${Math.floor(year)} years ago`;
   }
-  let month = time/(30*24*60*60);
+  var month = time/(30*24*60*60);
   console.log(month)
   if(month > 1){
     return `${Math.floor(month)} months ago`;
   }
-  let day = time/(24*60*60);
+  var day = time/(24*60*60);
   if(day > 1){
     return `${Math.floor(day)} days ago`;
   }
-  let hour = time/(60*60);
+  var hour = time/(60*60);
   console.log("hour",hour)
   if(hour > 1){
     return `${Math.floor(hour)} hours ago`;
   }
-  let minute = time/(60*60);
+  var minute = time/(60*60);
   if(minute > 1){
     return `${Math.floor(minute)} minutes ago`;
   }
-  let second = time/(60*60);
+  var second = time/(60*60);
   return `${Math.floor(second)} seconds ago`;
 }
 
 function escape(str) {
-  let div = document.createElement('div');
+  var div = document.createElement('div');
   div.appendChild(document.createTextNode(str));
   return div.innerHTML;
 }
