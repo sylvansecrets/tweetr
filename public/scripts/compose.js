e480fdea278b62470f60ae0d1b96ec655f12000b
Richard Zhang <lapislacrima@gmail.com>
2016-11-16 22:10:59 +0000

    increased box shadow when new tweet form is not hidden

--- public/scripts/compose.js
+++ public/scripts/compose.js
@@ -1,6 +1,11 @@
 $(document).ready(function(){
   $('.new-tweet').hide();
   $('#nav-bar').children('.compose').on('click', function(){
+    if($(this).hasClass('held')){
+      $(this).removeClass('held');
+    } else {
+      $(this).addClass('held');
+    }
     $('.new-tweet').slideToggle("fast", function(){
       $(this).children('form').children('textarea').focus();
     })
