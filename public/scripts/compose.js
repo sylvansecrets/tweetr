7e7716762d61dce31c54c1b64b23c9d5ffe4f0ff
Richard Zhang <lapislacrima@gmail.com>
2016-11-16 00:52:29 +0000

    deleted comments that contain dead code

--- public/scripts/compose.js
+++ public/scripts/compose.js
@@ -1,12 +1,4 @@
 $(document).ready(function(){
-  // $('#nav-bar').children('.compose').on('click', function(){
-  //   $newTweet = $('.new-tweet');
-  //   if ($newTweet.hasClass('collapsed')){
-  //     $newTweet.removeClass('collapsed');
-  //   } else {
-  //     $newTweet.addClass('collapsed')
-  //   }
-  // });
   $('.new-tweet').hide();
   $('#nav-bar').children('.compose').on('click', function(){
     $('.new-tweet').slideToggle("fast", function(){
