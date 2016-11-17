94a33bd58e8e825a86abfbb9624089cb6c218e37
Richard Zhang <lapislacrima@gmail.com>
2016-11-17 00:55:19 +0000

    moved all css into scss files

--- server/index.js
+++ server/index.js
@@ -8,8 +8,23 @@ const app         = express();
 const tweetsApi  = require('./api/tweets');
 const db         = require('./lib/db');
 
+var sassMiddleWare = require('node-sass-middleware');
+const path = require('path');
+
+app.use(sassMiddleWare({
+  src: path.join(__dirname, '..', 'sass'),
+  dest: path.join(__dirname, '..', 'public'),
+  debug: true,
+  outputStyle: 'compressed',
+  prefix: 'http://localhost:8080/styles'
+}))
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+
+
+
+
 
 db.connect((dbInstance) => {
   app.use('/tweets', tweetsApi(dbInstance));
